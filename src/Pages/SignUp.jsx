9bd3c0197986ebfe24433a7064127489deee2ef8
Muusi Nzyoka <muusinzyoka@Nzyokas-MacBook.local>
2024-08-13 14:00:12 +0300

    add success message

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -11,6 +11,7 @@ const SignUp = () => {
   const [year, setYear] = useState("");
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
+  const [successMessage, setSuccessMessage] = useState(""); // New state for success message
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -60,6 +61,12 @@ const SignUp = () => {
         setYear("");
         setMessage("");
         setLoading(false);
+        setSuccessMessage(
+          "Application submitted successfully. "
+        ); // Set success message
+
+     
+       
       })
       .catch((error) => {
         console.error("Error sending email", error);
@@ -73,6 +80,11 @@ const SignUp = () => {
     }
   };
 
+  const hideSuccessMessage = () => {
+    const successMessage = document.querySelector("#successMessage");
+    successMessage.style.opacity = "0";
+  }
+
   return (
     <motion.div
       initial={{ x: "-100vw" }}
@@ -81,6 +93,50 @@ const SignUp = () => {
       transition={{ duration: 1, ease: "easeInOut" }}
     >
       <div className="relative h-full flex justify-center">
+      {successMessage && (
+  <div id="successMessage" className="overlay opacity-1">
+    <div className="cards sm:h-[200px] lg:flex-row sm:flex-col">
+      
+    
+      <div className="icon-container">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          strokeWidth="0"
+          fill="currentColor"
+          stroke="currentColor"
+          className="icon"
+        >
+          <path 
+            d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"
+          ></path>
+        </svg>
+      </div>
+      <div className="message-text-container">
+        <p className="message-text">{successMessage}</p>
+        <p className="sub-text">Our publicity secretary will contact you with more information soon.</p>
+      </div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 15 15"
+        strokeWidth="0"
+        fill="none"
+        stroke="currentColor"
+        className="cross-icon"
+     onClick={hideSuccessMessage}/* Add your close functionality here */
+      >
+        <path
+          fill="currentColor"
+          d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
+          clipRule="evenodd"
+          fillRule="evenodd"
+        ></path>
+      </svg>
+    </div>
+  </div>
+)}
+
+
         <form
           onSubmit={handleSubmit}
           onKeyDown={handleKeyDown}
